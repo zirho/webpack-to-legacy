ee1412daafb8f3dfd9c4097d81ae4100b07c685e
Joshua <zirho6@gmail.com>
2016-08-14 14:07:01 -0700

    add new entry file to webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,6 +9,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/dev-server',
+    './src/entry.js'
   ],
   output: {
     path:       path.join(__dirname, 'dist'),
